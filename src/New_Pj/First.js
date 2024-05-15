39a7f7b061889328afafc6c1039a82743e0a8890
Naveen007-Jatdevta <nc06390@gmail.com>
2024-05-15 12:53:55 +0530

    new added cotent

---
+++ src/New_Pj/First.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './First.css'
+const First = () => {
+  return (
+    <div>
+      <div className='main'>
+        <p>asdfghjk lkjhgfdfghjoiuhy gfdfghjkhgfdcvbjkjhgfdfghj iuytredfghjkhgvcvbnmjhgfdfgyui  uytrdfghjhbvcfgygfsdfgfd fghjkjhgfdxcvhjhgfdfghjhgf dsxcfghjhgfdfghjhgfdfghjuy trfdtytrewqasxcvh jkoiuytrfd</p>
+      </div>
+    </div>
+  )
+}
+
+export default First
